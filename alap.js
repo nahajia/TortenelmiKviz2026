333680642ea2b5578567406e3878d279433a88e2
Aniko <nahajia@gmail.com>
2026-03-25 08:21:33 +0100

    kep 12darab

---
+++ alap.js
@@ -0,0 +1,13 @@
+
+let sz=""
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 4; j++) {
+        sz+=`
+        <img src="a0.jpg" alt="kep" class="kisKep">
+        `
+    }
+    sz+="<br>"
+}
+document.getElementById("kepekHelye").innerHTML=sz
+
+
